d07797eef874248ecdce1bead81c9a40d004d136
Mishad <mishadcs2002@gmail.com>
2024-10-07 16:43:28 +0600

    Day 2 [Data Types, Numbers, Operations, Type Conversion,f -string]

---
+++ Day 2 [Data Types, Numbers, Operations, Type Conversion,f -string] .py
@@ -0,0 +1,41 @@
+# Subscripting
+print ("Hello")
+
+# String
+print ("123" + "345")
+
+# Integer = Whole number
+print(123 + 345)
+
+#Large Integers
+print(123_456_789)
+
+#Boolean
+print(True)
+
+##Type Error, Type Checking and Type Conversion
+lenn = "Hello"
+print(type(lenn))
+print(type(123))
+print(type(12.33))
+print(type(True))
+
+a = int("123")
+x = len("sfkjhgsadf")
+print(x)
+
+#Mathematical Operations in Python
+print("My age : "+ str(12))
+print(123+456)
+print(7-3)
+print(3*2)
+print(5/3)
+print(5//3)
+print(2**3)
+
+#Number Manipulation and F Strings in Python
+bmi = 54/1.65**2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi,2))
